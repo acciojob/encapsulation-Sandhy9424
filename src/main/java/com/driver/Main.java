90f2d7d98a8b1b9674683e6dab9623d558acb9b4
Sandhy <tiwarisandhyprakash@gmail.com>
2023-02-16 15:31:25 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly reOnly=new RWOnly();
+        reOnly.setName("sa");
+        System.out.print(reOnly.getName());
+        //name has private access in com.driver.RWOnly
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name="Ayaush";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
